c67229523fd73921f6b3ec27f3c5f070d369794b
StaticBay <116987383+StaticBay@users.noreply.github.com>
2022-11-01 15:55:45 -0300

    Add files via upload

---
+++ perimetro-retangulo/src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double base, altura, area, areaRetangulo;
+
+        base = 11.55;
+        altura = 25;
+        areaRetangulo = base * altura;
+
+        System.out.println("A area de uma retangulo de base = " + base);
+        System.out.println("e altura " + altura);
+        System.out.println("é " + areaRetangulo);
+    }
+}
